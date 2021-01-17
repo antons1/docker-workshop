570eea28e848735ba226b715999aff8b84113081
Håkon Antonsen <haakon@antons1.net>
2021-01-17 09:55:05 +0100

    Fix backend project name

--- 04-docker-compose-and-volumes/backend/project.clj
+++ 04-docker-compose-and-volumes/backend/project.clj
@@ -1,4 +1,4 @@
-(defproject trip-collector "0.1.0-SNAPSHOT"
+(defproject docker-workshop "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
